cf7451b957a4561006b9054d972618d1eb747ef0
pranitdas <pranit.das@gmail.com>
2019-03-25 18:58:15 +0530

    testmaven25

---
+++ TestMaven25/src/main/java/TestMaven25/TestMaven25/App.java
@@ -0,0 +1,13 @@
+package TestMaven25.TestMaven25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
